c9a7e55c2221c54b84809de00fb137a210bfb642
Tom Young <tom.young@bp.com>
2021-10-08 16:05:15 +0100

    Bishop moves diagonally - red 4

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,11 +1,43 @@
-import Piece from './piece';
+//bishop
+import Square from "./../square";
+import Player from "../player";
+import Piece from "./piece";
 
 export default class Bishop extends Piece {
-    constructor(player) {
-        super(player);
+  constructor(player) {
+    super(player);
+  }
+  getAvailableMoves(board) {
+    const currentSquare = board.findPiece(this);
+    const bishopDirection = [];
+
+    for (let i = -7; i < 8; i++) {
+      if (
+        currentSquare.row + i < 8 &&
+        currentSquare.col + i < 8 &&
+        currentSquare.row + i >= 0 &&
+        currentSquare.col + i >= 0 &&
+        i !== 0
+      ) {
+        bishopDirection.push(
+          Square.at(currentSquare.row + i, currentSquare.col + i)
+        );
+      }
+    }
+    for (let i = -7; i < 8; i++) {
+      if (
+        currentSquare.row - i < 8 &&
+        currentSquare.col + i < 8 &&
+        currentSquare.row - i >= 0 &&
+        currentSquare.col + i >= 0 &&
+        i !== 0
+      ) {
+        bishopDirection.push(
+          Square.at(currentSquare.row - i, currentSquare.col + i)
+        );
+      }
     }
 
-    getAvailableMoves(board) {
-        return new Array(0);
-    }
+    return bishopDirection;
+  }
 }
