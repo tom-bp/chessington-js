5a9620f213dea8b1d3f02e13780278258e2fdd69
Tom Young <tom.young@bp.com>
2021-10-08 15:28:23 +0100

    rook moves in the way it should - red 3

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,11 +1,27 @@
-import Piece from './piece';
+//castle
+import Square from "./../square";
+import Player from "../player";
+import Piece from "./piece";
 
 export default class Rook extends Piece {
-    constructor(player) {
-        super(player);
+  constructor(player) {
+    super(player);
+  }
+  getAvailableMoves(board) {
+    const currentSquare = board.findPiece(this);
+    const rookDirection = [];
+
+    for (let i = -7; i < 8; i++) {
+      if (currentSquare.row + i < 8 && currentSquare.row + i >= 0 && i !== 0) {
+        rookDirection.push(Square.at(currentSquare.row + i, currentSquare.col));
+      }
+    }
+    for (let i = -7; i < 8; i++) {
+      if (currentSquare.col + i < 8 && currentSquare.col + i >= 0 && i !== 0) {
+        rookDirection.push(Square.at(currentSquare.row, currentSquare.col + i));
+      }
     }
 
-    getAvailableMoves(board) {
-        return new Array(0);
-    }
+    return rookDirection;
+  }
 }
