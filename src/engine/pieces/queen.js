89d561bc171ad382fad40cda6ad44d3a4fa1b760
Tom Young <tom.young@bp.com>
2021-10-08 16:11:56 +0100

    Queen moves in all directions - red 5

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -1,11 +1,56 @@
-import Piece from './piece';
+//Queen
+import Square from "./../square";
+import Player from "../player";
+import Piece from "./piece";
 
 export default class Queen extends Piece {
-    constructor(player) {
-        super(player);
-    }
+  constructor(player) {
+    super(player);
+  }
+  getAvailableMoves(board) {
+    const currentSquare = board.findPiece(this);
+    const queenDirection = [];
 
-    getAvailableMoves(board) {
-        return new Array(0);
+    for (let i = -7; i < 8; i++) {
+      if (
+        currentSquare.row + i < 8 &&
+        currentSquare.col + i < 8 &&
+        currentSquare.row + i >= 0 &&
+        currentSquare.col + i >= 0 &&
+        i !== 0
+      ) {
+        queenDirection.push(
+          Square.at(currentSquare.row + i, currentSquare.col + i)
+        );
+      }
     }
+    for (let i = -7; i < 8; i++) {
+      if (
+        currentSquare.row - i < 8 &&
+        currentSquare.col + i < 8 &&
+        currentSquare.row - i >= 0 &&
+        currentSquare.col + i >= 0 &&
+        i !== 0
+      ) {
+        queenDirection.push(
+          Square.at(currentSquare.row - i, currentSquare.col + i)
+        );
+      }
+    }
+    for (let i = -7; i < 8; i++) {
+      if (currentSquare.row + i < 8 && currentSquare.row + i >= 0 && i !== 0) {
+        queenDirection.push(
+          Square.at(currentSquare.row + i, currentSquare.col)
+        );
+      }
+    }
+    for (let i = -7; i < 8; i++) {
+      if (currentSquare.col + i < 8 && currentSquare.col + i >= 0 && i !== 0) {
+        queenDirection.push(
+          Square.at(currentSquare.row, currentSquare.col + i)
+        );
+      }
+    }
+    return queenDirection;
+  }
 }
